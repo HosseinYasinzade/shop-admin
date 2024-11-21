fbcaea7659efb801135dac49c5b26286d4e0589e
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-11-21 12:27:03 +0330

    end home design

--- src/Components/Form1.js
+++ src/Components/Form1.js
@@ -1,5 +1,13 @@
 function Form1() {
-  return <div></div>;
+  return (
+    <form>
+      <input type="text" required placeholder="Product" />
+      <input type="text" required placeholder="brand" />
+      <input type="text" required placeholder="Model" />
+      <input type="text" required placeholder="Color" />
+      <button>Go to next page</button>
+    </form>
+  );
 }
 
 export default Form1;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,7 @@ function Home({ items, setNewItem }) {
   return (
     <>
       <header className="header">
-        <h2>Panel</h2>
+        <h1>Panel</h1>
         <Link to="/form1" className="add-item-btn">
           Add Item
         </Link>
@@ -15,7 +15,14 @@ function Home({ items, setNewItem }) {
         {items.map((item) => {
           return (
             <div className="cart" key={item.id}>
-              <h2 className="title">{item.product}</h2>
+              <h2 className="title">Product : {item.product}</h2>
+              <p className="product-info-txt">Brand: {item.brand}</p>
+              <p className="product-info-txt">Color: {item.color}</p>
+              <p className="product-info-txt">Model: {item.model}</p>
+              <p className="product-info-txt">Summery: {item.summery}</p>
+              <p className="product-info-txt">Type: {item.type}</p>
+              <p className="product-info-txt">Size: {item.size}</p>
+              <p className="product-info-txt">Price: {item.price}</p>
             </div>
           );
         })}
