b65398bddbe054fe31ac599ab4e095a1e4b34c61
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-11-21 18:07:27 +0330

    add form2 ui

--- src/Components/Form2.js
+++ src/Components/Form2.js
@@ -1,5 +1,14 @@
 function Form2() {
-  return <div></div>;
+  return (
+    <form className="form-2">
+      <input type="text" required placeholder="Type" autoFocus />
+      <input type="number" required placeholder="Size" />
+      <input type="number" required placeholder="Price" />
+      <button type="submit" className="submit-1">
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default Form2;
