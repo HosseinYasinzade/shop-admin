88f14694cf675a25067c1c3e0373c7188cc8ff36
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-11-21 13:17:02 +0330

    add form 1

--- src/Components/Form1.js
+++ src/Components/Form1.js
@@ -1,11 +1,14 @@
 function Form1() {
   return (
-    <form>
-      <input type="text" required placeholder="Product" />
+    <form className="form-1">
+      <input type="text" required placeholder="Product" autoFocus />
       <input type="text" required placeholder="brand" />
       <input type="text" required placeholder="Model" />
       <input type="text" required placeholder="Color" />
-      <button>Go to next page</button>
+      <textarea placeholder="Summery"></textarea>
+      <button type="submit" className="submit-1">
+        Go to next page
+      </button>
     </form>
   );
 }
