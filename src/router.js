9788106c40e8a149f155fa122d2d294b71c3840f
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-11-23 10:08:14 +0330

    router change for better code

--- src/router.js
+++ src/router.js
@@ -2,32 +2,20 @@ import Home from "./Components/Home";
 import Form1 from "./Components/Form1";
 import Form2 from "./Components/Form2";
 
-function routers(items, setNewItem, handleSubmit, handleSubmit2) {
+function makeRoute(path, Component, props) {
+  return {
+    path,
+    element: <Component {...props} />,
+  };
+}
+
+function routers(items, setItems, handleSubmit, handleSubmit2) {
+  const props = { items, setNewItem: setItems };
+
   return [
-    {
-      path: "/",
-      element: <Home items={items} setNewItem={setNewItem} />,
-    },
-    {
-      path: "/form1",
-      element: (
-        <Form1
-          items={items}
-          setNewItem={setNewItem}
-          handleSubmit={handleSubmit}
-        />
-      ),
-    },
-    {
-      path: "/form2",
-      element: (
-        <Form2
-          items={items}
-          setNewItem={setNewItem}
-          handleSubmit2={handleSubmit2}
-        />
-      ),
-    },
+    makeRoute("/", Home, props),
+    makeRoute("/form1", Form1, { ...props, handleSubmit }),
+    makeRoute("/form2", Form2, { ...props, handleSubmit2 }),
   ];
 }
 
